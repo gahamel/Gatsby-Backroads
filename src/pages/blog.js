ee8f35b1c7fea805819597da5df4e54fb2d4eb88
Gaétan HAMEL <g.hamel@hotmail.fr>
2019-08-20 17:44:24 +0200

    first commit (91)

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'gatsby'
+import Layout from '../components/Layout';
+const blog = () => {
+  return (
+    <Layout>
+      hello from blog page<Link to="/">Back to home</Link>
+    </Layout>
+  )
+}
+
+export default blog
---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout';
+const contact = () => {
+  return (
+    <Layout>
+      Hello from contact page
+    </Layout>
+  )
+}
+
+export default contact
