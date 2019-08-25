66f65af78f9039cfea9ed8a22c4aad01743773ad
Gaétan HAMEL <g.hamel@hotmail.fr>
2019-08-25 15:32:34 +0200

    first contentful query

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Services from '../components/Home/Services'
 import StyledHero from '../components/StyledHero'
 import {graphql} from 'gatsby';
 import AniLink from 'gatsby-plugin-transition-link/AniLink';
+import FeaturedTours from '../components/Home/FeaturedTours';
 
 export default ({data}) => (
     <Layout>
@@ -16,6 +17,7 @@ export default ({data}) => (
       </StyledHero>
       <About />
       <Services />
+      <FeaturedTours />
     </Layout>
 )
 
